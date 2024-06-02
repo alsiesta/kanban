8c39861ad8fcb80bb38826e79d176b1c1d30070f
alsiesta <as@daslizenzatelier.de>
2024-06-02 17:52:06 +0200

    fixed environment dev vs. prod

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 
 @Component({
